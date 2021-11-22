a53631cd4e7c5f99b3e25998f13d464c27ce5744
aravind1217 <aravindsharma1217@gmail.com>
2021-11-22 15:00:14 +0530

    Reports1 page added

--- src/app/basic-ui/ReportsTable.js
+++ src/app/basic-ui/ReportsTable.js
@@ -1,93 +1,129 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../../tables.css'
-import { createMuiTheme } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
-import MUIDataTable from 'mui-datatables';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import '../../tables.css'
+// import { createMuiTheme } from '@material-ui/core';
+// import { ThemeProvider } from '@material-ui/styles';
+// import MUIDataTable from 'mui-datatables';
+// import { Link } from 'react-router-dom';
 
-const theme = createMuiTheme({
-  overrides: {
-    MuiTable: {
-      root: {
-        border: [[1, 'solid', '#d3d3d3']],
-      },
-    },
-    MuiTableCell: {
-      root: {
-        borderColor: '#d3d3d3',
-      },
-      head: {
-        color: '#474748',
-        fontSize: '18px',
-        background: '#fec107',
-        '&:not(:last-child)': {
-          borderRight: [[1, 'solid', '#c0c0c0']],
-        },
-      },
-    },
-    MuiTableSortLabel: {
-      root: {
-        alignItems: 'flex-start',
-      },
-    },
-    MuiTableFooter: {
-      root: {
-        background: 'white',
+// const theme = createMuiTheme({
+//   overrides: {
+//     MuiTable: {
+//       root: {
+//         border: [[1, 'solid', '#d3d3d3']],
+//       },
+//     },
+//     MuiTableCell: {
+//       root: {
+//         borderColor: '#d3d3d3',
+//       },
+//       head: {
+//         color: '#474748',
+//         fontSize: '18px',
+//         background: '#fec107',
+//         '&:not(:last-child)': {
+//           borderRight: [[1, 'solid', '#c0c0c0']],
+//         },
+//       },
+//     },
+//     MuiTableSortLabel: {
+//       root: {
+//         alignItems: 'flex-start',
+//       },
+//     },
+//     MuiTableFooter: {
+//       root: {
+//         background: 'white',
         
         
-      },
-    },
+//       },
+//     },
 
-    // MUIDataTable
-    MUIDataTableHeadCell: {
-      sortLabelRoot: {
-        // height: undefined,
-      },
-    },
-  },
-});
+//     // MUIDataTable
+//     MUIDataTableHeadCell: {
+//       sortLabelRoot: {
+//         // height: undefined,
+//       },
+//     },
+//   },
+// });
 
-const columns = [
-  { name: 'id', label: 'S.No' },
-  { name: 'project_name', label: 'Project Name' },
-  { name: 'project_source_id', label: 'Project Source ID' },
-  {
-    name: 'analysis_type',
-    label: 'Analysis Type',
+// const columns = [
+//   { name: 'id', label: 'S.No' },
+//   { name: 'project_name', 
+//   label: 'Project Name',
+  
+  
+
+// },
+//   { name: 'project_source_id', label: 'Project Source ID' },
+//   {
+//     name: 'analysis_type',
+//     label: 'Analysis Type',
     
-  },
-  { name: 'status', label: 'Status' },
-];
-
-const data = [
+//   },
+//   { name: 'status', label: 'Status' },
   
-  {
-    id: '1',
-    project_name: 'INDTNA12345_re1',
-    project_source_id: 'INDTNA12345',
-    analysis_type: 'TO pipeline',
-    status: 'Completed'
-  },
   
-];
+// ];
 
-function Tables() {
+// const data = [
+  
+//   {
+//     id: '1',
+//     project_name: 'INDTNA12345_re1',
+   
+    
+//     project_source_id: 'INDTNA12345',
+//     analysis_type: 'TO pipeline',
+//     status: 'Completed'
+//   },
+  
+// ];
+
+// function Tables() {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <MUIDataTable
+//         columns={columns}
+//         data= {data}  
+//         options={{
+        
+//           responsive: '',
+//           fixedHeader: false,
+//           filterType: 'textField',
+//           selectableRows: 'none',
+//           elevation: 0,
+//           print: false,
+//         }}
+//       />
+//     </ThemeProvider>
+//   );
+// }
+
+// export default Tables
+
+
+import React from 'react'
+import { Helmet } from 'react-helmet'
+const ReportsTable = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <MUIDataTable
-        columns={columns}
-        data={data}
-        options={{
-          responsive: '',
-          fixedHeader: false,
-          filterType: 'textField',
-          selectableRows: 'none',
-          elevation: 0,
-          print: false,
-        }}
-      />
-    </ThemeProvider>
-  );
+    <div>
+      <Helmet>
+     
+  
+     
+
+
+ 
+
+
+
+
+      </Helmet>
+      
+    </div>
+  )
 }
 
-export default Tables
+export default ReportsTable
